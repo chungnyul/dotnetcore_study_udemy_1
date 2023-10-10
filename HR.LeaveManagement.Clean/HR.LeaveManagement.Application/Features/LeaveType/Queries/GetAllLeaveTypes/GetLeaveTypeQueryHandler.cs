55d60a8043aa1f704af2c526be9fd4f91b6d0c8b
chung <chungnyul@gmail.com>
2023-10-10 22:32:42 +0900

    20강 수정 완료; 빌드 성공

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypeQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class GetLeaveTypeQueryHandler : IRequestHandler<GetLeaveTypeQuery, List<LeaveTypeDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public GetLeaveTypeQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+        }
+
+
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeQuery request, CancellationToken cancellationToken)
+        {
+            // Query the database
+            var leaveTypes = await _leaveTypeRepository.GetAsync();
+
+            // Convert data objects to DTO objects
+            var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+            return data;
+        }
+    }
+}
