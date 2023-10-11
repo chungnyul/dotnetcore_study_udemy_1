5b1a7c2cbc5ee07f529e976f8ed39baec973ae8d
chung <chungnyul@gmail.com>
2023-10-11 23:11:14 +0900

    DeleteLeaveTypeCommandHandler.cs 강좌 완료. 그러나, 강좌와 다르게 수정한 내용이 있음.

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // Retrieve domain entity object;
+            var leaveTypeToDelete = _leaveTypeRepository.GetByIdAsync(request.Id);
+
+            // verify that record exist;
+
+
+            // remove from database;
+
+            // 원래 강좌에 있는 내용
+            //await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete); 
+
+            // 변환 에러에 따른 수정한 내용
+            var data = _mapper.Map<Domain.LeaveType>(leaveTypeToDelete);
+            await _leaveTypeRepository.DeleteAsync(data);
+
+
+            // ruturn record id;
+            return Unit.Value;
+
+        }
+    }
+}
---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // Validate incoming data;
+
+
+            // Convert to damain object
+            var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+
+            // update database
+            await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
+
+            // return Unit value;
+            return Unit.Value;
+
+        }
+    }
+}
