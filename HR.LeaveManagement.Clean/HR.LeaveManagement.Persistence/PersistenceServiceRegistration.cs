7e7380a2d6a45d657b47d502dfb6b2c801a65ebe
chung <chungnyul@gmail.com>
2023-10-14 11:31:30 +0900

    31강 완료

--- HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +12,14 @@ namespace HR.LeaveManagement.Persistence;
 
 public static class PersistenceServiceRegistration
 {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<HrDatabaseContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+        });
+
+
         return services;
     }
 }
