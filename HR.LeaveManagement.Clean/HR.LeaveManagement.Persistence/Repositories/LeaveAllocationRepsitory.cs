7c9852b09b45581381ba071913c3d72d6a5e4726
chung <chungnyul@gmail.com>
2023-10-14 12:12:58 +0900

    32강 완료

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/Repositories/LeaveAllocationRepsitory.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveAllocationRepsitory : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+    {
+        public LeaveAllocationRepsitory(HrDatabaseContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRespository.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveRequestRespository : GenericRepository<LeaveRequest>, ILeaveRequesteRepository
+    {
+        public LeaveRequestRespository(HrDatabaseContext context) : base(context)
+        {
+        }
+    }
+}
