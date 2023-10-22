0cc990f1630f306ea143497a619d23eb29561365
chung <chungnyul@gmail.com>
2023-10-22 09:29:55 +0900

    43강 완료. DB 연결은 connection string 없다고 실패됨.

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Api/Controllers/LeaveTypesController.cs
@@ -0,0 +1,53 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveTypesController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        // GET: api/<LeaveTypesController>
+        [HttpGet]
+        public async Task<List<LeaveTypeDto>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypeQuery());
+            return leaveTypes;
+        }
+
+        // GET api/<LeaveTypesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LeaveTypesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LeaveTypesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LeaveTypesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
