beb7a507550dc1892292c2ec7fb2d719e485322f
chung <chungnyul@gmail.com>
2023-10-22 11:20:25 +0900

    45강 완료

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/GetLeaveAllocationDetailRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using HR.LeaveManagement.Application.Exceptions;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails
+{
+    public class GetLeaveAllocationDetailRequestHandler : IRequestHandler<GetLeaveAllocationDetailQuery, LeaveAllocationDetailsDto>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveAllocationDetailRequestHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<LeaveAllocationDetailsDto> Handle(GetLeaveAllocationDetailQuery request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.GetLeaveAllocationWithDetails(request.Id);
+            if ( leaveAllocation == null)
+            {
+                throw new NotFoundException(nameof(LeaveAllocation), request.Id);
+            }
+
+            return _mapper.Map<LeaveAllocationDetailsDto>(leaveAllocation);
+        }
+    }
+}
