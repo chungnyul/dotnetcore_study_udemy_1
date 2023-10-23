720520ed53f495545f1a0a36a2fd160a89d9f4ab
chung <chungnyul@gmail.com>
2023-10-23 23:23:30 +0900

    45강 완료

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/DeleteLeaveAllocation/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation
+{
+    public class DeleteLeaveAllocationCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand, Unit>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveAllocationCommandHandler(
+            ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper )
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.GetByIdAsync(request.Id);
+
+            if ( leaveAllocation == null )
+            {
+                throw new NotFoundException(nameof(leaveAllocation), request.Id);
+            }
+
+            await _leaveAllocationRepository.DeleteAsync(leaveAllocation);
+            return Unit.Value;
+
+        }
+    }
+}
